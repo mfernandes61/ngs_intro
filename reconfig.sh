c4e9c4b9a44b1a1a332db2d6680a305cd3bbf57d
mfernandes61 <mfernandes61@gmail.com>
2016-04-28 14:22:47 +0100

    Update reconfig.sh

--- reconfig.sh
+++ reconfig.sh
@@ -3,3 +3,10 @@
 # set -e
 
 echo "Reetting environment for NGSINTRO from defaults"
+SIAB_VERSION=2.19 
+SIAB_ADDUSER=true
+SIAB_USER=ngsintro
+SIAB_GROUP=ngsintro 
+SIAB_PASSWORD=ngsintro
+SIAB_HOME=course
+SIAB_SUDO=true 
